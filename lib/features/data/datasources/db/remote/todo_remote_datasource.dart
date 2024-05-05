7b6b1473d765b1138cccf9208da61c7af63b23a4
JonatanaRandria <hei.jonatana@gmail.com>
2024-05-05 22:40:20 +0300

    chore: implement todo ui

--- lib/features/data/datasources/db/remote/todo_remote_datasource.dart
+++ lib/features/data/datasources/db/remote/todo_remote_datasource.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart' hide Headers;
 import 'package:retrofit/retrofit.dart';
 import 'package:zara_ao/core/constants/Constants.dart';
 import 'package:zara_ao/features/data/model/todo.dart';
-
+import 'package:zara_ao/core/constants/Constants.dart';
 
 part 'todo_remote_datasource.g.dart';
 
--- lib/features/data/repository/TodoRepository.dart
+++ lib/features/data/repository/TodoRepository.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' show Client;
+import 'package:zara_ao/features/data/model/todo.dart';
+import 'package:zara_ao/core/constants/Constants.dart';
 
 class TodoRepository{
-Client client = Client();
-
-  Future<List<>>
 
 }
