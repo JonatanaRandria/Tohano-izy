d4a3e95f8a74e76329981717dfc04aecb41ccf00
JonatanaRandria <hei.jonatana@gmail.com>
2024-05-06 08:44:08 +0300

    chore: add todo widgets

--- lib/features/presentation/pages/home/home_page.dart
+++ lib/features/presentation/pages/home/home_page.dart
@@ -5,6 +5,38 @@ import 'package:flutter/widgets.dart';
 
 
 
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+   
+      appBar:
+       AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+       
+        title: Text(widget.title),
+      ),
+      body: const HomePage() 
+
+    );
+  }
+}
+
+
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
